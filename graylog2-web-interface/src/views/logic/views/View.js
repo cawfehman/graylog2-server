a5bb326c2358012bb83064e2db060b95e6be27e8
Linus Pahl <lpahl@protonmail.com>
2020-04-20 16:51:52 +0200

    Fix linter warnings and errors

--- graylog2-web-interface/src/views/logic/views/View.js
+++ graylog2-web-interface/src/views/logic/views/View.js
@@ -218,7 +218,7 @@ export default class View {
   }
 }
 
-type InternalBuilderState = Immutable.Map<string, any>
+type InternalBuilderState = Immutable.Map<string, any>;
 class Builder {
   value: InternalBuilderState;
 
